c904c2775873c853099d5267da50802e9f97b65c
Martin Kaller <marka727@student.liu.se>
2024-03-06 14:45:24 +0000

    #2 Setup express.js with typescript and swagger

---
+++ backend/src/controllers/ping.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Route } from "tsoa";
+
+interface PingResponse {
+  message: string;
+}
+
+@Route("ping")
+export class PingController extends Controller {
+  @Get("/")
+  public getMessage(): PingResponse {
+    return {
+      message: "hello",
+    };
+  }
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,81 @@
+/**
+ * Setup express server.
+ */
+
+import morgan from "morgan";
+import path from "path";
+import helmet from "helmet";
+import express, { Request, Response, NextFunction } from "express";
+import logger from "jet-logger";
+
+import "express-async-errors";
+
+import EnvVars from "@src/constants/EnvVars";
+import HttpStatusCodes from "@src/constants/HttpStatusCodes";
+
+import { NodeEnvs } from "@src/constants/misc";
+import { RouteError } from "@src/other/classes";
+
+import swaggerUi from "swagger-ui-express";
+
+import { RegisterRoutes } from "@src/routes";
+
+// **** Variables **** //
+
+const app = express();
+
+// **** Setup **** //
+
+// Basic middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Show routes called in console during development
+if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
+  app.use(morgan("dev"));
+}
+
+// Security
+if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
+  app.use(helmet());
+}
+
+// Add error handler
+app.use(
+  (
+    err: Error,
+    _: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction,
+  ) => {
+    if (EnvVars.NodeEnv !== NodeEnvs.Test.valueOf()) {
+      logger.err(err, true);
+    }
+    let status = HttpStatusCodes.BAD_REQUEST;
+    if (err instanceof RouteError) {
+      status = err.status;
+    }
+    return res.status(status).json({ error: err.message });
+  },
+);
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  }),
+);
+
+// Set static directory
+const staticDir = path.join(__dirname, "public");
+app.use(express.static(staticDir));
+
+// Register swagger routes
+RegisterRoutes(app);
+
+// **** Export default **** //
+export default app;
