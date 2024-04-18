fa10f7e7d7bd3e867b00f19b1059d5cabe9abd9b
kaller01 <martin@kallers.se>
2024-04-18 11:48:28 +0200

    run prettier

--- common/src/masterPlan.ts
+++ common/src/masterPlan.ts
@@ -1,30 +1,30 @@
 import { Course, Ecv, PeriodName } from "./studieinfo";
 
 export interface MasterPlan {
-    programName: string;
-    startYear: string;
-    semesters: SemesterPlan[];
-    specializion: string;
-    note: string;
+  programName: string;
+  startYear: string;
+  semesters: SemesterPlan[];
+  specializion: string;
+  note: string;
 }
 
 export interface SemesterPlan {
-    name: string;
-    periods: PeriodPlan[];
+  name: string;
+  periods: PeriodPlan[];
 }
 
 export interface PeriodPlan {
-    name: PeriodName;
-    courses: Course[];
+  name: PeriodName;
+  courses: Course[];
 }
 
 export interface PlannedCourse extends Course {
-    semester: null | string;
-    specializations: PlannedCourseSpecialization[];
-    note: string;
+  semester: null | string;
+  specializations: PlannedCourseSpecialization[];
+  note: string;
 }
 
 export interface PlannedCourseSpecialization {
-    name: string;
-    ECV: Ecv;
+  name: string;
+  ECV: Ecv;
 }
